bde39f94e030873d4a243603d7c358545beff413
khoirulanwar7 <muhammadkhoirulanwarudin7.gmail.com>
2022-11-28 04:44:06 +0700

    Commit navbar fixed

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,19 @@
 const hamburger = document.querySelector('#hamburger')
+const navMenu = document.querySelector('nav-mene')
 
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active')
+    hamburger.classList.toggle('hidden')
 })
+
+window.onscroll = function(){
+    const header = document.querySelector('header')
+    const fixedNav = header.offsetTop;
+
+    if(window.pageYOffset> fixedNav){
+        header.classList.add('navbar-fixed')
+    }else{
+        header.classList.remove('navbar-fixed')
+    }
+}
+
